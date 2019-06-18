e24b46f1e16abb4893563998251b4fcd37710bad
deividlsantos <deividluiz.santos@hotmail.com>
2019-06-18 14:37:17 -0300

    sixth commit

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -15,6 +15,9 @@ class UserController extends Controller
 
     public function profileUpdate(Request $request)
     {
+
+    	$user = auth()->user();
+
     	$data = $request->all();
 
     	if ($data['password'] != null)
@@ -22,7 +25,28 @@ class UserController extends Controller
     	else
     		unset($data['password']);
 
-    	$update = auth()->user()->update($data);
+    	$data['image'] = $user->image;
+    	if ($request->hasFile('image') && $request->file('image')->isValid()) {
+    		if ($user->image)
+    			$name = $user->image;
+    		else
+    			$name = $user->id.kebab_case($user->name);
+
+    		$extenstion = $request->image->extension();
+    		$nameFile = "{$name}.{$extenstion}";
+
+    		$data['image'] = $nameFile;
+
+    		$upload = $request->image->storeAs('users', $nameFile);
+
+    		if (!$upload)
+    			return redirect()
+    						->back()
+    						->with('error', 'Falha ao fazer upload da imagem');
+
+    	}
+
+    	$update = $user->update($data);
 
     	if ($update)
     		return redirect()
--- resources/views/site/profile/profile.blade.php
+++ resources/views/site/profile/profile.blade.php
@@ -18,7 +18,7 @@
 	</div>
 @endif
 
-<form action="{{ route('profile.update') }}" method="POST">
+<form action="{{ route('profile.update') }}" method="POST" enctype="multipart/form-data">
 	{!! csrf_field() !!}
 	<div class="form-group">
 		<label for="name">Nome</label>
@@ -33,7 +33,9 @@
 		<input type="password" name="password" placeholder="Senha" class="form-control">
 	</div>
 	<div class="form-group">
-		<label for="image">Imagem:</label>
+		@if (auth()->user()->image != null)
+			<img src="{{ url('storage/users/'.auth()->user()->image) }}" alt="{{ auth()->user()->name }}" style="max-width: 15%;">
+		@endif
 		<input type="file" name="image" class="form-control">
 	</div>
 
